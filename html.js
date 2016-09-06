77e26b0cd5fc44b292799b74899bd077188ef5ad
Jean-Chrsitophe Delmas <jeanchristophe@reach5.co>
2016-09-06 22:20:39 +0200

    add google analytics tag

--- html.js
+++ html.js
@@ -34,6 +34,15 @@ module.exports = React.createClass({
           <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css' />
           <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' rel='stylesheet' type='text/css' />
           {css}
+
+          <script dangerouslySetInnerHTML={{ __html: `
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-83770119-1', 'auto');
+            ga('send', 'pageview');
+          ` }}></script>
         </head>
         <body>
           <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
